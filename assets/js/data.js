7f7ad68761e39e64761919ecccf952f1e90fea6d
임예지 <image4362@gmail.com>
2026-05-15 07:47:11 +0900

    splash, cards 이미지 교체

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,27 +1,27 @@
 window.CARDS_DATA = [
   {
     id: 1, type: 'student', name: "김가은",
-    bgImage: "assets/images/cards/card-01.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "강사님 안녕하세요.<br> 스승의 날을 기념하여<br> 이렇게 편지를 작성해봅니다.<br> 말주변이 없어 잘 표현을 못했지만<br> 육아나 개인적인 일이 바쁘심에도 불구하고<br> 항상 열심히 강의를 가르쳐주시고<br> 저희를 위해 다방면으로 노력해주셔서 감사하게 생각하고 있습니다.<br> 얼마남지 않았지만 앞으로도 잘 부탁드리며<br> 나중이 되어서도 좋은 인연으로 만나뵐 수 있다면 좋겠습니다. 감사합니다."
   },
   {
     id: 2, type: 'student', name: "김수연",
-    bgImage: "assets/images/cards/card-02.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "선생님. 항상 밝은 에너지로 좋은 가르침 주셔서 감사합니다!<br> 덕분에 공부하는데 많은 도움이 되었어요.<br> 수업도 이해하기 쉽게 알려주셔서 편하게 배울 수 있었습니다.<br> 스승의 날 축하드립니다!!<br> - 김수연드림 -"
   },
   {
     id: 3, type: 'student', name: "김정수",
-    bgImage: "assets/images/cards/card-03.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "시작은 무작정 도전해보자란 마음으로 시작했지만 힘들긴 하네요.<br> 그래도 이왕 도전한거 선생님만 믿고 끝까지<br> 이 수업을 마쳐보겠습니다.<br> 감사합니다!"
   },
   {
     id: 4, type: 'student', name: "김지원",
-    bgImage: "assets/images/cards/card-04.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "안녕하세요. 쌤 저 지원입니다ㅋㅋ<br> 어느덧 5월 중순을 내다보고 있네요.<br> 2월, 3월엔 정말 서로가 다 어색해서 다들 대답도 제대로 안했었는데<br> 쌤이 대답 좀 해라.. 라고 하신 말에 진심이 느껴졌어요ㅋㅋㅋ<br> 그래서 그 후로부터 제 나름(?) 대답을 열심히 하려고 했던 게 생각납니다.. 허허.<br> 스승의 날 편지 쓰는 게 상당히 오랜만인데. 지친 일상 속 저희 편지로 환기 하셨음 좋겠네요. 화이팅.<br> 저의 목표가 하나 있습니다.<br> 선생님께서 수업 중 가끔씩 언급하시는<br> '전에 수강생 중 ~~~한 애가 ~~~취업 잘해서 지금 어디가있어~~~'의 '걔'가 되는 거예요. 하하<br> 열심히 하겠슴다 항상 감사합니다<br>p.s 아가 너무 기여워요!"
   },
   {
     id: 5, type: 'maker', name: "김주은",
-    bgImage: "assets/images/cards/card-01.png",
+    bgImage: "assets/images/cards/card-light.png",
     role: "FE · 디자인 · 기획",
     contribution: 30,
     teamBio: "프론트엔드 개발과 디자인, 기획까지 폭넓게 함께했어요.",
@@ -32,12 +32,12 @@ window.CARDS_DATA = [
   },
   {
     id: 6, type: 'student', name: "김태연",
-    bgImage: "assets/images/cards/card-02.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "To. 강현명<br> 스승의 날을 맞이하여 간략하게 짧게라도<br> 감사인사를 전하고자 합니다.<br> 훈련과정이 쉽지 않지만 그래도 덕분에 하나씩 해내고 있어요.<br> 남은 기간도 잘 부탁드립니다.<br> 태연 올림."
   },
   {
     id: 7, type: 'maker', name: "신혜인",
-    bgImage: "assets/images/cards/card-03.png",
+    bgImage: "assets/images/cards/card-light.png",
     role: "그래픽 · Figma · AI 음악",
     contribution: 25,
     teamBio: "그래픽 디자인(특히 Figma 작업)과 AI 음악 생성 등을 맡았어요.",
@@ -48,7 +48,7 @@ window.CARDS_DATA = [
   },
   {
     id: 8, type: 'maker', name: "윤태경",
-    bgImage: "assets/images/cards/card-04.png",
+    bgImage: "assets/images/cards/card-light.png",
     role: "FE · JavaScript",
     contribution: 20,
     teamBio: "프론트엔드 중에서도 JavaScript로 동작을 구현하는 부분을 중심으로 맡았어요.",
@@ -59,17 +59,17 @@ window.CARDS_DATA = [
   },
   {
     id: 9, type: 'cheerleader', name: "이기훈",
-    bgImage: "assets/images/cards/card-01.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "선생님,<br>매일 같은 강의내용을 반복하는 일이 쉽지 않으셨을 텐데<br>언제나 열정적으로 가르쳐주시고 끝까지 믿어주셔서 감사합니다.<br><br>선생님 덕분에 공부 이상의 많은 것을 배웠습니다.<br>할 수 있다는 자신감을 갖게 해주셔서 정말 감사합니다.<br><br>앞으로도 선생님께 배운 마음 잊지 않고 더 노력하겠습니다. 항상 건강하시고 행복하세요.<br>진심으로 존경합니다."
   },
   {
     id: 10, type: 'student', name: "이선규",
-    bgImage: "assets/images/cards/card-02.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "안녕하세요 선생님<br> 항상 저희를 위해 열정적으로 가르쳐주셔서 감사드립니다.<br> 이 수업을 통해 실무는 어찌 진행되고<br> 실무적으로 어떻게 해야하는지 알려주셔서 감사합니다.<br> 남은 기간 동안에도 잘 부탁드려요"
   },
   {
     id: 11, type: 'maker', name: "임예지",
-    bgImage: "assets/images/cards/card-03.png",
+    bgImage: "assets/images/cards/card-light.png",
     role: "리드 · 기획 · PM",
     contribution: 35,
     teamBio: "팀 리드와 기획, 그리고 프로젝트 전반을 총괄·조율했어요.",
@@ -80,17 +80,17 @@ window.CARDS_DATA = [
   },
   {
     id: 12, type: 'student', name: "최신비",
-    bgImage: "assets/images/cards/card-04.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "안녕하세요 :) 신비에요<br> 우선, 부족한 저희를 이끌어 주시느라 정말 고생이 많으세요..!<br> 컴퓨터라고는 타자밖에 못치던 제가 이만큼이나 왔다는건, 잘 가르쳐 주신 덕분이에요.<br> 관심과 흥미없이 그저 바른생활 목적으로 학원을 다닌 저에게 이만큼 발전의 기회를 주셔서 정말 감사드립니다.<br> 아침잠이 많아 자주 빠져서 죄송합니다..ㅠㅠ<br> 그래도 중도포기는 안하고 열심히 다녀보려구용 진짜에요.<br> 항상 필요한것만 알아듣기 쉽게 설명해주시고 선생님의 밝은 에너지가 느껴져서 좋아요 :)<br> 아직 아무것도 잘하는게 없지만, 남은 시간도 가르쳐주신 만큼 열심히 따라가겠습니다!!"
   },
   {
     id: 13, type: 'student', name: "박연수",
-    bgImage: "assets/images/cards/card-01.png",
+    bgImage: "assets/images/cards/card-light.png",
     message: "선생님 짧은 기간이지만 항상 많은 걸 알려주려고 하셔서 감사합니다.<br> 항상 감사한 마음입니다.<br> 행복한 스승의 날 되세요!"
   },
   {
     id: 14, type: 'special', name: "만든이들",
-    bgImage: "assets/images/cards/card-special.png",
+    bgImage: "assets/images/cards/card-secret.png",
     message: "선생님, 저희가 함께 만든 작은 선물이에요.<br>항상 감사합니다.",
     link: "https://example.com"
   }
